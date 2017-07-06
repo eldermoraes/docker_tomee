96374bd5299211a55821af2867f942b592a1d27c
Elder Moraes <elder@eldermoraes.com>
2017-07-06 13:49:16 -0300

    Static discovery challenge...

--- step3-tomee_static_discovery/run.sh
+++ step3-tomee_static_discovery/run.sh
@@ -12,15 +12,18 @@ sleep 3
 docker run -d --name tomee-soujava1 \
 	-h tomee-soujava1 \
 	--link mysql-soujava:mysql-soujava \
+	--network=soujava-network --ip 172.28.5.1 \
         -p 8081:8080 tomee-soujava
 
 docker run -d --name tomee-soujava2 \
         -h tomee-soujava2 \
         --link mysql-soujava:mysql-soujava \
+	--network=soujava-network --ip 172.28.5.2 \
         -p 8082:8080 tomee-soujava
 
 docker run -d --name tomee-soujava3 \
         -h tomee-soujava3 \
         --link mysql-soujava:mysql-soujava \
+	--network=soujava-network --ip 172.28.5.3 \
         -p 8083:8080 tomee-soujava
 
