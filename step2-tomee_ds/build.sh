498c58dbec55038a80af113e27848c97cbe91307
Elder Moraes <elder.moraes@gmail.com>
2017-08-19 13:49:06 -0300

    Removido o cp do apptest.war

--- step2-tomee_ds/build.sh
+++ step2-tomee_ds/build.sh
@@ -1,7 +1,7 @@
 docker rmi -f tomee-soujava
 docker rmi -f mysql-soujava
 
-cp ../apptest/target/apptest-1.0.war apptest.war
+# cp ../apptest/target/apptest-1.0.war apptest.war
 
 docker build -t tomee-soujava .
 
